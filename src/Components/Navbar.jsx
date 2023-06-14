95dcda7a08cf419267aa8763d7263f30f0cc9472
hong-yang-12 <nyanlinhtet2244@gmail.com>
2023-06-14 17:02:23 +0630

    base file

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { HiMenu } from "react-icons/hi";
+import { BiCaretDown } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+  };
+
+  return (
+    <>
+      <section className="w-full flex items-center justify-between sm:justify-around p-3">
+        <div className="m-3">
+          <img
+            className=" w-20"
+            src="https://static.pandamr.com/fs/userFiles/pandamrv2/images/logo.png"
+            alt=""
+          />
+        </div>
+        <div className="lg:w-[30rem] flex items-center lg:justify-between">
+          <nav className="hidden lg:flex gap-4 ">
+            <p>SOLUTIONS</p>
+            <p>CONTACT US</p>
+            <p>BLOG</p>
+          </nav>
+          <div className="w-56 flex justify-between gap-4">
+            <button
+              onClick={toggleMenu}
+              id="menuBtn"
+              className="lg:hidden text-4xl"
+            >
+              <HiMenu />
+            </button>
+            <button className="border border-gray-600 rounded-full px-6 py-1 hover:bg-gray-600 hover:text-gray-100">
+              Login
+            </button>
+
+            <div className="hidden lg:inline-block relative">
+              <select className="block py-2.5 px-0 w-24 text-sm bg-transparent appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                <option value="US">US</option>
+                <option value="CA">UK</option>
+                <option value="FR">Singapore</option>
+                <option value="DE">Germany</option>
+              </select>
+
+              <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                <BiCaretDown />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        id="mobileMenu"
+        className="absolute top-0 bg-gray-200 w-full text-xl  flex-col justify-center origin-top animate-open-menu hidden"
+      >
+        <button onClick={toggleMenu} className="text-6xl self-end px-6">
+          &times;
+        </button>
+        <nav
+          className="flex flex-col min-h-screen items-center py-8"
+          aria-label="mobile"
+        >
+          <h4 className="w-full text-center py-6 hover:opacity-90">Home</h4>
+          <h4 className="w-full text-center py-6 hover:opacity-90">
+            Solutions
+          </h4>
+          <h4 className="w-full text-center py-6 hover:opacity-90">
+            Contact us
+          </h4>
+          <h4 className="w-full text-center py-6 hover:opacity-90">Blog</h4>
+          <div className="inline-block relative bg-gray-100 p-2 ">
+            <select className="block py-2.5 px-0 w-24 text-lg bg-transparent appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+              <option value="US">US</option>
+              <option value="CA">UK</option>
+              <option value="FR">Singapore</option>
+              <option value="DE">Germany</option>
+            </select>
+
+            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+              <BiCaretDown />
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+};
+
+export default Navbar;
