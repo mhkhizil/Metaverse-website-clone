eb647a5dd24fcff3f3e0ebed67005a2a5d8f1b14
Wai-Min-Hein <wai06276@gmail.com>
2023-06-17 16:45:32 +0630

    solution and blog finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import ContactUs from './Components/ContactUs/ContactUs'
 import Login from './Components/Login/Login'
 import LoginEmail from './Components/Login/LoginEmail'
+import Soultion from './Components/Solution'
+import Blog from './Components/Blog'
 
 const App = () => {
   const location = useLocation()
@@ -15,6 +17,8 @@ const App = () => {
         <Route path='/contactus' element={<ContactUs/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/loginemail' element={<LoginEmail/>}/>
+        <Route path={`/blog/page/:id`} element={<Blog/>}/>
+        <Route path='/solution' element={<Soultion/>}/>
       </Routes>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      
+      'bmd': '810px',
+      // => @media (min-width: 810px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1537px',
+      // => @media (min-width: 1537px) { ... }
+    },
     extend: {
       keyframes:{
         'open-menu' : {
