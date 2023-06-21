182436e3a95831896dd96fe7731b3749ac8284fc
hong-yang-12 <nyanlinhtet2244@gmail.com>
2023-06-21 23:45:35 +0630

    home responsive done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,30 +16,32 @@ import { FaTelegramPlane } from "react-icons/fa";
 import Footer from "../Components/Footer";
 import { motion } from "framer-motion";
 
-
 const Home = () => {
-
   const imgVairants = {
-    open: {opacity:1, scale:1, transition: {duration:.5, delay:.3}},
-    closed: {opacity:0, scale:0.9, transition: {duration:.5, delay:.3}},
-  }
+    open: { opacity: 1, scale: 1, transition: { duration: 0.5, delay: 0.3 } },
+    closed: {
+      opacity: 0,
+      scale: 0.9,
+      transition: { duration: 0.5, delay: 0.3 },
+    },
+  };
 
   const divVairants = {
-    open: {opacity:1, x:0, transition: {duration:.5}},
-    closed: {opacity:0, x:-80, transition: {duration:.5}},
-  }
+    open: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    closed: { opacity: 0, x: -80, transition: { duration: 0.5 } },
+  };
 
   return (
     <div className=" mt-16 h-screen">
       <div className=" flex items-center justify-center">
-        <div className=" flex-col  items-center justify-center  w-4/5  ">
-          <p className=" text-center font-mainf text-5xl lin tracking-wide ">
+        <div className=" flex-col  items-center justify-center sm:w-4/5  ">
+          <p className=" text-center font-mainf text-4xl sm:text-5xl tracking-wide ">
             THE ULTIMATE METAVERSE
           </p>
-          <p className=" text-center font-mainf text-5xl tracking-wide mt-6  mb-2">
+          <p className=" text-center font-mainf text-4xl sm:text-5xl tracking-wide mt-6  mb-2">
             FOR BUSINESSES
           </p>
-          <p className=" text-center text-3xl text-gray-700 tracking-wider">
+          <p className=" text-center text-xl sm:text-3xl text-gray-700 tracking-wider">
             Your metaverse development company
           </p>
           <img
@@ -60,15 +62,15 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div className=" mt-32 flex justify-center items-center bg-black  ">
-        <div className="   flex justify-center items-center  w-4/5 ">
+      <div className=" mt-20 sm:mt-32 flex justify-center items-center bg-black  ">
+        <div className="sm:flex justify-around sm:justify-center items-center w-full sm:w-4/5">
           <div className="  ">
             <div className="m-9">
               <p className="  text-center text-white text-4xl font-extrabold">
                 Business
               </p>
             </div>
-            <div className="px-2 py-16 m-7 h-[350px] rounded-large   bg-blackistgrey">
+            <div className="px-2 py-16 m-7 sm:h-[350px] rounded-large text-sm   bg-blackistgrey">
               <div>
                 <p className=" text-white">
                   <TiTick className=" inline   text-lime-600 text-4xl" />
@@ -94,7 +96,7 @@ const Home = () => {
                 Education
               </p>
             </div>
-            <div className="  m-7 h-[350px] px-2 py-16  rounded-large bg-blackistgrey">
+            <div className="m-7 sm:h-[350px] px-2 py-16  rounded-large text-sm bg-blackistgrey">
               <div className="">
                 <p className="  text-white">
                   <TiTick className=" inline   text-lime-600 text-4xl" /> &nbsp;
@@ -126,11 +128,12 @@ const Home = () => {
               community
             </p>
             <motion.div
-            initial={'closed'}
-            whileInView={'open'}
-            variants={imgVairants}
-            viewport={{ once: true }}
-            className=" my-10">
+              initial={"closed"}
+              whileInView={"open"}
+              variants={imgVairants}
+              viewport={{ once: true }}
+              className=" my-10"
+            >
               <img
                 src="https://static.pandamr.com/fs/userFiles/pandamrv2/images/3514-l-2-2-16668542124443.webp"
                 alt=""
@@ -138,10 +141,10 @@ const Home = () => {
             </motion.div>
             <div className=" gap-10  flex items-center justify-center">
               <motion.div
-              initial={'closed'}
-              whileInView={'open'}
-              variants={imgVairants}
-              viewport={{ once: true }}
+                initial={"closed"}
+                whileInView={"open"}
+                variants={imgVairants}
+                viewport={{ once: true }}
               >
                 <img
                   className=" w-auto h-auto "
@@ -150,10 +153,10 @@ const Home = () => {
                 />
               </motion.div>
               <motion.div
-              initial={'closed'}
-              whileInView={'open'}
-              variants={imgVairants}
-              viewport={{ once: true }}
+                initial={"closed"}
+                whileInView={"open"}
+                variants={imgVairants}
+                viewport={{ once: true }}
               >
                 <img
                   className=" w-auto h-auto "
@@ -162,10 +165,10 @@ const Home = () => {
                 />
               </motion.div>
               <motion.div
-              initial={'closed'}
-              whileInView={'open'}
-              variants={imgVairants}
-              vierPort={{once:true}}
+                initial={"closed"}
+                whileInView={"open"}
+                variants={imgVairants}
+                vierPort={{ once: true }}
               >
                 <img
                   className=" w-auto h-auto "
@@ -178,11 +181,11 @@ const Home = () => {
         </div>
       </div>
       <div className=" flex-col items-center justify-center bg-black  ">
-        <div className=" pt-8  ">
-          <div className=" text-center text-gray-50 font-extrabold text-5xl">
+        <div className=" pt-20 sm:pt-8  ">
+          <div className=" text-center text-gray-50 font-extrabold text-4xl sm:text-5xl">
             METACOMMUNITY
           </div>
-          <div className=" text-xl text-center text-gray-50  tracking-widest my-4">
+          <div className=" text-lg sm:text-xl text-center text-gray-50  tracking-widest my-4">
             Join our community built around so many bright minds
           </div>
           <div className="py-8 gap-36 flex items-center justify-center text-gray-50">
@@ -196,14 +199,15 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <motion.div 
-      initial={'closed'}
-      whileInView={'open'}
-      variants={divVairants}
-      vierPort={{once:true}}
-      className=" mt-10 flex items-center justify-center">
-        <div className=" flex  gap-28 items-center justify-center  w-4/5  ">
-          <div className=" w-2/4">
+      <motion.div
+        initial={"closed"}
+        whileInView={"open"}
+        variants={divVairants}
+        vierPort={{ once: true }}
+        className=" mt-10 flex items-center justify-center"
+      >
+        <div className=" sm:flex  gap-28 items-center justify-center  w-4/5  ">
+          <div className=" sm:w-2/4">
             <div>
               <p className="  text-lg font-light">
                 Before you jump into corporate meta-spaces, think of your
@@ -221,7 +225,7 @@ const Home = () => {
               </button>
             </div>
           </div>
-          <div className=" w-2/4">
+          <div className=" mt-4 sm:w-2/4">
             <div className="relative">
               <img
                 src="https://static.pandamr.com/fs/userFiles/pandamrv2/images/0014007853502-artboard-1-16651470897565.webp"
@@ -233,7 +237,9 @@ const Home = () => {
               </div>
             </div>
           </div>
+          
         </div>
+        
       </motion.div>
       <div className=" mb-6  mt-20 flex  gap-28 items-center justify-center  ">
         <p className=" text-center font-mainf text-5xl lin tracking-wide ">
@@ -243,36 +249,36 @@ const Home = () => {
 
       <div className="  bg-black  ">
         <div className="   ">
-          
-         <img src="https://static.pandamr.com/fs/userFiles/pandamrv2/images/3380-partners-logos-pandamr.webp" alt="" />
-           
-        
+          <img
+            src="https://static.pandamr.com/fs/userFiles/pandamrv2/images/3380-partners-logos-pandamr.webp"
+            alt=""
+          />
         </div>
       </div>
       <div className=" my-10 flex items-center justify-center">
         <div className=" flex-col  items-center justify-center  w-4/5  ">
-          <p className=" text-center font-mainf text-5xl lin tracking-wide ">
-          Want to learn more about business implementations of Metaverse?
+          <p className=" text-center font-mainf text-xl sm:text-5xl lin tracking-wide ">
+            Want to learn more about business implementations of Metaverse?
           </p>
-          <p className="  text-center font-mainf text-5xl tracking-wide mt-6  mb-10">
-          Searching for ideas specifically for your business needs?
+          <p className="  text-center font-mainf text-xl sm:text-5xl tracking-wide mt-6  mb-10">
+            Searching for ideas specifically for your business needs?
           </p>
-          <p className=" text-center text-2xl text-gray-700 tracking-wider">
-          Tell us more about your business model and we will come up with a metaverse strategy that guarantees additional value for your company and team.
+          <p className=" text-center text-sm sm:text-2xl text-gray-700 tracking-wider">
+            Tell us more about your business model and we will come up with a
+            metaverse strategy that guarantees additional value for your company
+            and team.
           </p>
-         
+
           <div className=" text-center ">
             <button className="  hover:bg-hbg text-center bg-pg  rounded-large mt-10  px-20 py-3">
               <span className="">Submit a request</span>{" "}
             </button>
           </div>
-      
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
 
 export default Home;
-
